4fcf1b2e6b7f3f7a83dc6209646bfc2caf4eaf38
Jonas Lima de Amorim <dev.amorim@outlook.com>
2023-12-01 00:18:05 -0300

    First app version

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,276 @@
-import Image from 'next/image'
+"use client";
+
+import moment from "moment";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const holidays = (year) => {
+  var epochDate = moment("18990101", "YYYYMMDD");
+  return [
+    {
+      month: 1,
+      day: 1,
+      name: "Confraternização Universal",
+      moment: moment(`${year}/01/01`, "YYYY/MM/DD"),
+      epoch: moment(`${year}/01/01`, "YYYY/MM/DD").diff(epochDate, "days") + 1,
+    },
+    {
+      month: 1,
+      day: 25,
+      name: "Aniversário de São Paulo",
+      moment: moment(`${year}/01/25`, "YYYY/MM/DD"),
+      epoch: moment(`${year}/01/25`, "YYYY/MM/DD").diff(epochDate, "days") + 1,
+    },
+    {
+      month: 4,
+      day: 20,
+      name: "Tiradentes",
+      moment: moment(`${year}/04/20`, "YYYY/MM/DD"),
+      epoch: moment(`${year}/04/20`, "YYYY/MM/DD").diff(epochDate, "days") + 1,
+    },
+    {
+      month: 5,
+      day: 1,
+      name: "Dia do Trabalho",
+      moment: moment(`${year}/05/01`, "YYYY/MM/DD"),
+      epoch: moment(`${year}/05/01`, "YYYY/MM/DD").diff(epochDate, "days") + 1,
+    },
+    {
+      month: 9,
+      day: 7,
+      name: "Independência do Brasil",
+      moment: moment(`${year}/09/07`, "YYYY/MM/DD"),
+      epoch: moment(`${year}/09/07`, "YYYY/MM/DD").diff(epochDate, "days") + 1,
+    },
+    {
+      month: 10,
+      day: 12,
+      name: "Nossa Senhora Aparecida",
+      moment: moment(`${year}/10/12`, "YYYY/MM/DD"),
+      epoch: moment(`${year}/10/12`, "YYYY/MM/DD").diff(epochDate, "days") + 1,
+    },
+    {
+      month: 11,
+      day: 2,
+      name: "Finados",
+      moment: moment(`${year}/11/02`, "YYYY/MM/DD"),
+      epoch: moment(`${year}/11/02`, "YYYY/MM/DD").diff(epochDate, "days") + 1,
+    },
+    {
+      month: 11,
+      day: 15,
+      name: "Proclamação da República",
+      moment: moment(`${year}/11/15`, "YYYY/MM/DD"),
+      epoch: moment(`${year}/11/15`, "YYYY/MM/DD").diff(epochDate, "days") + 1,
+    },
+    {
+      month: 11,
+      day: 20,
+      name: "Consciência Negra",
+      moment: moment(`${year}/11/20`, "YYYY/MM/DD"),
+      epoch: moment(`${year}/11/20`, "YYYY/MM/DD").diff(epochDate, "days") + 1,
+    },
+    {
+      month: 12,
+      day: 25,
+      name: "Natal",
+      moment: moment(`${year}/12/25`, "YYYY/MM/DD"),
+      epoch: moment(`${year}/12/25`, "YYYY/MM/DD").diff(epochDate, "days") + 1,
+    },
+  ];
+};
+
+const zeroPad = (num, places) => String(num).padStart(places, "0");
+
+const calculateCalendar = (year, month) => {
+  var daysInMonth = [
+    31,
+    year % 4 == 0 ? 29 : 28,
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31,
+  ];
+
+  var daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  var monthIndex = month - 1;
+
+  var monthArray = [];
+
+  for (var i = 1; i <= daysInMonth[monthIndex]; i++) {
+    var monthDate = moment(
+      `${year}${zeroPad(month, 2)}${zeroPad(i, 2)}`,
+      "YYYYMMDD"
+    );
+
+    var epochDate = moment("18990101", "YYYYMMDD");
+
+    var daysSinceEpoch = monthDate.diff(epochDate, "days") + 1;
+
+    var dayOfWeekIndex = daysSinceEpoch % 7;
+
+    monthArray.push({
+      epoch: daysSinceEpoch,
+      dayOfWeek: daysOfWeek[dayOfWeekIndex],
+      dayOfWeekIndex: dayOfWeekIndex,
+      date: monthDate.date(),
+    });
+  }
+  return monthArray;
+};
+
+const monthInfos = (year, month) => {
+  const calendar = calculateCalendar(year, month);
+  const hollidays = holidays(year).filter((x) => x.month === month);
+
+  var weekDays = calendar.filter(
+    (x) => x.dayOfWeekIndex > 0 && x.dayOfWeekIndex < 6
+  );
+
+  var weekendDays = calendar.filter(
+    (x) => x.dayOfWeekIndex === 0 || x.dayOfWeekIndex === 6
+  );
+
+  const hollidaysInWeekdays = hollidays.map((x) =>
+    weekDays.reduce((count, y) => {
+      if (y.epoch === x.epoch) {
+        return (count += 1);
+      }
+      return count;
+    }, 0)
+  );
+
+  const hollidaysInWeekends = hollidays.map((x) =>
+    weekendDays.reduce((count, y) => {
+      if (y.epoch === x.epoch) {
+        return (count += 1);
+      }
+      return count;
+    }, 0)
+  );
+
+  return {
+    totalDays: calendar.length,
+    weekDays: weekDays.length,
+    weekendDays: weekendDays.length,
+    hollidays: hollidays.length,
+    hollidaysInWeekdays: hollidaysInWeekdays.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    ),
+    hollidaysInWeekends: hollidaysInWeekends.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    ),
+  };
+};
 
 export default function Home() {
+  let [month, setMonth] = useState(moment().month() + 1);
+  let [year, setYear] = useState(moment().year());
+  let [calendar, setCalendar] = useState([]);
+  let [hollidays, setHollidays] = useState([]);
+  let [infos, setInfos] = useState(null);
+  let [displayDate, setDisplayDate] = useState("");
+
+  useEffect(() => {
+    const newDisplayDate = moment(
+      `${year}${zeroPad(month, 2)}01`,
+      "YYYYMMDD"
+    ).format("MMMM YYYY");
+
+    setDisplayDate(newDisplayDate);
+    setCalendar(calculateCalendar(year, month));
+    setInfos(monthInfos(year, month));
+    setHollidays(holidays(year).filter((x) => x.month === month));
+  }, [month, year]);
+
+  function currentMonth() {
+    setMonth(moment().month());
+    setYear(moment().year());
+  }
+
+  function changeMonth(increment) {
+    var currentMonth = month + increment;
+    var currentYear = year;
+
+    if (currentMonth < 1) {
+      currentMonth = 12;
+      currentYear -= 1;
+    }
+
+    if (currentMonth > 12) {
+      currentMonth = 1;
+      currentYear += 1;
+    }
+
+    setMonth(currentMonth);
+    setYear(currentYear);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="min-h-screen items-center px-4 w-1/3 mx-auto">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="font-bold text-4xl">{displayDate}</h1>
+
+        <div className="flex gap-2">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => changeMonth(-1)}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Previous
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => currentMonth()}
+          >
+            Current
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => changeMonth(1)}
+          >
+            Next
+          </button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        {infos !== null && (
+          <div className="mb-4">
+            <p>Days in month: {infos.totalDays}</p>
+            <p>Working days: {infos.weekDays - infos.hollidaysInWeekdays}</p>
+            <p>Week days: {infos.weekDays}</p>
+            <p>Weekend days: {infos.weekendDays}</p>
+            <p>Hollidays during the week: {infos.hollidaysInWeekdays}</p>
+            <p>Hollidays during the weekend: {infos.hollidaysInWeekends}</p>
+          </div>
+        )}
+        {hollidays.length > 0 && (
+          <div>
+            <h1 className="mb-2 font-bold text-xl">Next hollidays</h1>
+            {hollidays.map((x) => (
+              <p key={uuidv4()}>
+                {x.moment.format("DD/MM/YYYY - dddd")} - {x.name}
+              </p>
+            ))}
+          </div>
+        )}
       </div>
     </main>
-  )
+  );
 }
